426f9c10e7c5869a463ebc48c140c86b21198cdb
MasterGTFX <baran.dominik98@gmail.com>
2020-01-20 19:33:20 +0100

    additional changes

--- register/models.py
+++ register/models.py
@@ -3,12 +3,24 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.exceptions import ObjectDoesNotExist
+from shop.models import ProductInCart
 
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     balance = models.PositiveIntegerField(default=0)
+    shopping_cart = models.ManyToManyField(ProductInCart, blank=True)
+
+    @property
+    def shopping_cart_price(self):
+        cart_sum = 0
+        for product_in_cart in self.shopping_cart:
+            cart_sum += product_in_cart.product.price * product_in_cart.product_count
+        return cart_sum
+
+    def __str__(self):
+        return "{}".format(self.user)
 
 
 @receiver(post_save, sender=User)
