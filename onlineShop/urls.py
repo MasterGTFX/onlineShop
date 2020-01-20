d32e4ef767890ad1b09521d1dc4dc41fde46ebc6
MasterGTFX <baran.dominik98@gmail.com>
2020-01-20 19:14:20 +0100

    merged

--- onlineShop/urls.py
+++ onlineShop/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +26,4 @@ urlpatterns = [
     path('register/', include('register.urls')),
     path('login/',  views.LoginView.as_view(), name='login',),
     path('', include("django.contrib.auth.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
